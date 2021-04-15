4cc66db328e901da363c32ba85f1f213c3e9a2c8
RiadSleiman <riad54sleiman@gmail.com>
2021-04-15 11:22:45 +0200

    My fourth commit

--- tictactoe/js/app.js
+++ tictactoe/js/app.js
@@ -16,7 +16,40 @@ cols.forEach((col) =>{
             let symbol = document.querySelector('.p'+current_player+'_symbol').innerHTML;
             this.innerHTML = symbol;
 
+            // Check winner
+            let latWinCount = 0;
+            let longWinCount = 0
+            for(let i=0 ; i < 3 ; i++){
+                for(let j=0 ; j < 3; j++){
+                    
+                    if(document.querySelector(".id"+i+j).innerHTML == symbol){
+                        latWinCount++;
+                    }
+                    if(document.querySelector(".id"+j+i).innerHTML == symbol){
+                        longWinCount++;
+                    }
 
+                    if(latWinCount == 3 || longWinCount == 3){
+                        console.log("Player "+current_player+" wins");
+                    }
+                    
+                }
+                latWinCount = 0;
+                longWinCount = 0;
+            }
+
+            if(document.querySelector(".id00") == symbol && document.querySelector(".id11") == symbol && document.querySelector(".id22") == symbol
+                || document.querySelector(".id02") == symbol && document.querySelector(".id11") == symbol && document.querySelector(".id20") == symbol){
+                    console.log("Player "+current_player+" wins");
+                }
+
+
+            if(latWinCount == 3 || longWinCount == 3){
+                console.log("Player "+current_player+" wins");
+            }
+            
+
+            document.querySelector(".player"+current_player).classList.remove("active");
 
             //Switch the current player after playing
             if (current_player == 1){
@@ -26,10 +59,7 @@ cols.forEach((col) =>{
                 current_player = 1;
             }
 
-            let iactivePlayer = document.querySelector(".player:not(.active)");
-            let activePlayer = document.querySelector(".player.active");
-            iactivePlayer.classList.add("active");
-            activePlayer.classList.remove("active");
+            document.querySelector(".player"+current_player).classList.add("active");
         }
         else{
             alert('Box is full!!!');
