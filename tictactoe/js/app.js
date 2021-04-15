8cc798e83b7c44bd4727173eaedc4e28d82a5d28
RiadSleiman <riad54sleiman@gmail.com>
2021-04-15 10:52:26 +0200

    My third commit

--- tictactoe/js/app.js
+++ tictactoe/js/app.js
@@ -1,9 +1,10 @@
-//console.log('Hello World!');
-//alert('Hello World');
+/*console.log('Hello World!');
+alert('Hello World');
 
-//let table = document.getElementById('table_game');
+let table = document.getElementById('table_game');
 
-//table.style.background = "pink";
+table.style.background = "pink";*/
+
 let current_player = 1;
 let cols = document.querySelectorAll(".col");
 
@@ -11,14 +12,24 @@ cols.forEach((col) =>{
     
     col.onclick = function(){
         if(this.innerHTML == ''){
+            // Put the symbol in the col
             let symbol = document.querySelector('.p'+current_player+'_symbol').innerHTML;
             this.innerHTML = symbol;
+
+
+
+            //Switch the current player after playing
             if (current_player == 1){
                 current_player = 2;
             }   
             else{
                 current_player = 1;
             }
+
+            let iactivePlayer = document.querySelector(".player:not(.active)");
+            let activePlayer = document.querySelector(".player.active");
+            iactivePlayer.classList.add("active");
+            activePlayer.classList.remove("active");
         }
         else{
             alert('Box is full!!!');
