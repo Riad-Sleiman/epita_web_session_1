c29d6cf3705dff34addac5e6a72613a1d1a4e28c
RiadSleiman <riad54sleiman@gmail.com>
2021-04-19 16:15:52 +0200

    ninth commit

--- tictactoe/js/app.js
+++ tictactoe/js/app.js
@@ -8,13 +8,19 @@ winLabel.style.display = 'none';
 function onClickPlay(){
     localStorage.setItem('pl1', document.getElementById("p1name").value);
     localStorage.setItem('pl2', document.getElementById("p2name").value);
+    localStorage.setItem("score1",0);
+    localStorage.setItem("score2",0);
+    
 }
 
+document.querySelector('.p1_name').innerHTML = localStorage.getItem('pl1') + " "+ localStorage.getItem("score1");
+document.querySelector('.p2_name').innerHTML = localStorage.getItem('pl2') + " "+ localStorage.getItem("score2");
 
 cols.forEach((col) =>{
     
     col.onclick = function(){
         if(this.innerHTML == '' && isPlaying){
+
             // Put the symbol in the col
             let symbol = document.querySelector('.p'+current_player+'_symbol').innerHTML;
             this.innerHTML = symbol;
@@ -36,11 +42,14 @@ cols.forEach((col) =>{
                     if(latWinCount == 3 || longWinCount == 3){
                         isPlaying = false;
                         winLabel.style.display = '';
-                        if(symbol == "X")
+                        if(symbol == "X"){
                             winLabel.querySelector('.player').innerHTML = localStorage.getItem('pl1')+" wins. Click to restart";
-                        else
+                            localStorage.setItem("score1", parseFloat(localStorage.getItem("score1"))+1);
+                        }
+                        else{
                             winLabel.querySelector('.player').innerHTML = localStorage.getItem('pl2')+" wins. Click to restart";
-                        console.log("Player "+current_player+" wins");
+                            localStorage.setItem("score2", parseFloat(localStorage.getItem("score2"))+1);
+                        }
                     }
                     
                 }
@@ -52,10 +61,14 @@ cols.forEach((col) =>{
             if(document.querySelector(".id00").innerHTML == symbol && document.querySelector(".id11").innerHTML == symbol && document.querySelector(".id22").innerHTML == symbol
                 || document.querySelector(".id02").innerHTML == symbol && document.querySelector(".id11").innerHTML == symbol && document.querySelector(".id20").innerHTML == symbol){
                     winLabel.style.display = '';
-                    if(symbol == "X")
-                            winLabel.querySelector('.player').innerHTML = localStorage.getItem('pl1')+" wins. Click to restart";
-                        else
-                            winLabel.querySelector('.player').innerHTML = localStorage.getItem('pl2')+" wins. Click to restart";
+                    if(symbol == "X"){
+                        winLabel.querySelector('.player').innerHTML = localStorage.getItem('pl1')+" wins. Click to restart";
+                        localStorage.setItem("score1", parseFloat(localStorage.getItem("score1"))+1);
+                    }
+                    else{
+                        winLabel.querySelector('.player').innerHTML = localStorage.getItem('pl2')+" wins. Click to restart";
+                        localStorage.setItem("score2", parseFloat(localStorage.getItem("score2"))+1);
+                    }
                     isPlaying=false;
                 }       
 
