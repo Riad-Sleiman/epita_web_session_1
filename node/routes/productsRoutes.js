4bbac493e833d0bdad4eb1c85b4ad85967f82efc
RiadSleiman <riad54sleiman@gmail.com>
2021-04-26 14:47:15 +0200

    12th commit

---
+++ node/routes/productsRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const {v4: uuidv4} = require('uuid')
+
+//placeholder database
+const products = []
+
+router.get('/', (req,res) => {
+    res.status(200).json(products)
+})
+
+router.get('/:index', (req,res) => {
+    res.status(200).send(products[req.params.index])
+})
+
+router.post('/', (req,res) => {
+    let product = req.body
+    product.id = uuidv4()
+    products.push(product)
+
+    res.status(200).send(product)
+})
+
+router.put('/:productId', (req,res) => {
+    const productId = req.params.productId
+    const productIndex = products.indexOf(productId)
+    let product = req.body
+    product.id = uuidv4()
+    products.splice(productIndex,1,product)
+    res.status(200).send('Product update page')
+})
+
+router.delete('/:productId', (req,res) => {
+    const productId = req.params.productId
+    const productIndex = products.indexOf(productId)
+    products.splice(productIndex,1)
+    console.log(products)
+    res.status(200).send('Product delete page')
+})
+
+module.exports = router
--- node/server.js
+++ node/server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 
-const app = express()       
+const productRoutes = require('./routes/productsRoutes')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
+app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.status(200).send('It works !')  //status 200 : all is good
+    res.status(200).send('It works !')
 })
+
+app.use('/products',productRoutes)
 
 app.listen(3000, () => {
     console.log(`Server listen on http://localhost:3000`)
